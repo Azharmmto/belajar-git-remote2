c185b27010f403f40e9745d711bb5d114423fa0b
Azharmmto <azharmamonto7@gmail.com>
2024-07-04 16:54:31 +0800

    added file menghitung luas segitiga

---
+++ luasSegitiga.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+float luasSeggitiga(float alas, float tinggi);
+
+int main(){
+
+  // rumus luas segitiga
+  // L = 1/2 * a * t
+      // a : alas
+      // t : tinggi
+
+  float alas, tinggi;
+
+  cout << "Masukkan alas segitiga (cm): ";
+  cin >> alas;
+
+  cout << "Masukkan tinggi segitiga (cm): ";
+  cin >> tinggi;  
+
+  return 0;
+}
