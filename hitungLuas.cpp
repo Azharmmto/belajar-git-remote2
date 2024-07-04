61bbd5cbeef80a13bbcddf3238ed5d6fb1b68c1e
Azharmmto <azharmamonto7@gmail.com>
2024-07-04 14:48:23 +0800

    initial commit

---
+++ hitungLuas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int luas(int panjang, int lebar){
+  return panjang * lebar;
+}
+
+int main(){
+
+  int panjang, lebar, hasil;
+
+  std::cout << "Masukkan panjang: ";
+  std::cin >> panjang;
+
+  std::cout << "Masukkan lebar: ";
+  std::cin >> lebar;
+
+  hasil = luas(panjang, lebar);
+
+  std::cout << "================================================================" << std::endl;
+
+  std::cout << "Luas persegi panjang dengan lebar " << lebar <<" dan panjang " << panjang << " adalah " << hasil << std::endl;
+
+  std::cout << "================================================================" << std::endl;
+}
